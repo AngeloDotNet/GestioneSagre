031e50c6d29f3be32bad5c004ca0703fab3510e0
Angelo Pirola <angelo@aepservice.it>
2022-06-10 15:37:49 +0200

    Refactoring Program e Startup

--- src/GestioneSagre.Web/Server/Program.cs
+++ src/GestioneSagre.Web/Server/Program.cs
@@ -1,26 +1,22 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿namespace GestioneSagre.Web.Server;
 
-namespace GestioneSagre.Web.Server
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+        // Vari esempi per usare il nuovo builder: https://docs.microsoft.com/en-us/aspnet/core/migration/50-to-60-samples
+        WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        Startup startup = new(builder.Configuration);
+
+        // Aggiungere i servizi per la dependency injection (metodo ConfigureServices)
+        startup.ConfigureServices(builder.Services);
+
+        WebApplication app = builder.Build();
+
+        // Usiamo i middleware (metodo Configure)
+        startup.Configure(app);
+
+        app.Run();
     }
 }
