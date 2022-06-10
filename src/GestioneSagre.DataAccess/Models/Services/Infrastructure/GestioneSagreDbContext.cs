b8ececa7dff3aaf8686abaa5efe535125426fea3
Angelo Pirola <angelo@aepservice.it>
2022-06-10 16:33:02 +0200

    Aggiunta migration VERSIONE

--- src/GestioneSagre.DataAccess/Models/Services/Infrastructure/GestioneSagreDbContext.cs
+++ src/GestioneSagre.DataAccess/Models/Services/Infrastructure/GestioneSagreDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using GestioneSagre.Models.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace GestioneSagre.DataAccess.Models.Services.Infrastructure
 {
@@ -8,9 +9,18 @@ namespace GestioneSagre.DataAccess.Models.Services.Infrastructure
         {
         }
 
+        public virtual DbSet<Versione> Versioni { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
+            //Tabella VERSIONE
+            modelBuilder.Entity<Versione>(entity =>
+            {
+                entity.ToTable("Versione");
+                entity.HasKey(versione => versione.Id);
+            });
         }
     }
 }
---
+++ src/GestioneSagre.Models/Entities/Versione.cs
@@ -0,0 +1,19 @@
+﻿namespace GestioneSagre.Models.Entities
+{
+    public class Versione
+    {
+        public int Id { get; private set; }
+        public string CodiceVersione { get; private set; } = string.Empty;
+        public string TestoVersione { get; private set; } = string.Empty;
+
+        public void ChangeCodiceVersione(string codiceVersione)
+        {
+            CodiceVersione = codiceVersione;
+        }
+
+        public void ChangeTestoVersione(string testoVersione)
+        {
+            TestoVersione = testoVersione;
+        }
+    }
+}
