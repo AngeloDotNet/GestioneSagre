67a2e6aa5f90083d17931180d69e3d0fe3f2e7b4
Angelo Pirola <angelo@aepservice.it>
2022-06-11 13:35:44 +0200

    Abilitato Global Using - GestioneSagre.DataAccess

--- src/GestioneSagre.DataAccess/Models/Services/Infrastructure/GestioneSagreDbContext.cs
+++ src/GestioneSagre.DataAccess/Models/Services/Infrastructure/GestioneSagreDbContext.cs
@@ -1,26 +1,22 @@
-﻿using GestioneSagre.Models.Entities;
-using Microsoft.EntityFrameworkCore;
+﻿namespace GestioneSagre.DataAccess.Models.Services.Infrastructure;
 
-namespace GestioneSagre.DataAccess.Models.Services.Infrastructure
+public partial class GestioneSagreDbContext : DbContext
 {
-    public partial class GestioneSagreDbContext : DbContext
+    public GestioneSagreDbContext(DbContextOptions<GestioneSagreDbContext> options) : base(options)
     {
-        public GestioneSagreDbContext(DbContextOptions<GestioneSagreDbContext> options) : base(options)
+    }
+
+    public virtual DbSet<Versione> Versioni { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        //Tabella VERSIONE
+        modelBuilder.Entity<Versione>(entity =>
         {
-        }
-
-        public virtual DbSet<Versione> Versioni { get; set; }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-
-            //Tabella VERSIONE
-            modelBuilder.Entity<Versione>(entity =>
-            {
-                entity.ToTable("Versione");
-                entity.HasKey(versione => versione.Id);
-            });
-        }
+            entity.ToTable("Versione");
+            entity.HasKey(versione => versione.Id);
+        });
     }
 }
