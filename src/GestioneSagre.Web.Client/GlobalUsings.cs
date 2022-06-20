682644cf7d19286e271a63fcd792666e17a50d91
Angelo Pirola <angelo@aepservice.it>
2022-06-20 16:37:59 +0200

    Caricamento assembly Client tramite Lazy Loading #54

--- src/GestioneSagre.Web.Client/GlobalUsings.cs
+++ src/GestioneSagre.Web.Client/GlobalUsings.cs
@@ -1,3 +1,7 @@
-﻿global using GestioneSagre.Web.Client.Shared;
+﻿global using System.Reflection;
+global using GestioneSagre.Web.Client.Shared;
+global using Microsoft.AspNetCore.Components;
+global using Microsoft.AspNetCore.Components.Routing;
 global using Microsoft.AspNetCore.Components.Web;
 global using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+global using Microsoft.AspNetCore.Components.WebAssembly.Services;
